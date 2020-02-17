a4d3217bb589549ef4da8e8d57829486cb494568
zhouyijun <zhouyijun@ruijie.com.cn>
2020-02-17 22:24:52 +0800

    手写apply和bind

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import styles from './styles/index.less';
 
 import  './pollfill/index'
 function f(param) {
+    console.log(arguments);
     this.a =param
 }
 const obj={};
+// const newF = f.mockbind(obj,'3', '2', '1')
+// newF('a', 'b', 'c')
+
 f.mockApply(
     obj,['p']
 )
