ca4fdb7f552fa6d146f94b08aabce287bc0ba3b4
zhouyijun <zhouyijun@ruijie.com.cn>
2020-02-22 17:00:06 +0800

    引入react

--- src/index.js
+++ src/index.js
@@ -2,25 +2,8 @@ import html from './index.html'
 import styles from './styles/index.less';
 // import styles from './styles/styles.css';
 import "@babel/polyfill";
+import App from "./app";
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-
-import Counter from './counter';
-import Number from "./number";
-Counter();
-new Number();
-
-if(module.hot) {
-  module.hot.accept('./number', () => {
-      document.body.removeChild(document.getElementById('number'));
-      new Number();
-  })
-}
-new Promise((resolve, reject) => {
-    resolve('helle')
-}).then((res) => {
-    console.log(res)
-})
-
-[1,2,3].map((item) => {
-    return item + 1;
-})
+ReactDOM.render(<App/>, document.getElementById('box'));
--- src/number.js
+++
@@ -1,9 +0,0 @@
-export default class Number {
-    constructor(props) {
-        const div = document.createElement('div');
-        div.id = 'number';
-        div.innerText = '100';
-        document.body.appendChild(div);
-    }
-
-}
