904b31346f89118d11b37af8c4fe94d89253575d
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-02 18:35:42 +0800

    思考引用变量修改的影响

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import html from './index.html'
 import styles from './styles/index.less';
 
 
+const outerObject = {
+    innerObject:{
+        a:1
+    }
+};
+
+let innerObject = outerObject.innerObject;
+outerObject.innerObject.a = 3; //这种方式修改两个变量值都会修改
+console.log(innerObject, outerObject);
+
+// outerObject.innerObject = null;
+innerObject = {
+    c:2
+}; // 这种直接改变引用变量指向不会修改同样指向的引用变量
+console.log(innerObject, outerObject);
 
