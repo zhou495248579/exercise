c9b24a09328783293439aef4f8583e02de418440
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-10 23:54:17 +0800

    节流防抖

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import html from './index.html'
 import styles from './styles/index.less';
 
 import  './pollfill/index'
-function f(param) {
-    console.log(arguments);
-    this.a =param
-}
-const obj={};
-// const newF = f.mockbind(obj,'3', '2', '1')
-// newF('a', 'b', 'c')
+import {debounce, throttle} from "./pollfill";
 
-f.mockApply(
-    obj,['p']
-)
-console.log(obj)
+window.addEventListener('scroll', throttle(() => {
+    console.log('scroll')
+}, 500));
