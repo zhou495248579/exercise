6a265a162ca7b5f8fdb580e7da1259e063ea3c5b
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-10 23:54:17 +0800

    节流防抖

--- src/pollfill/index.js
+++ src/pollfill/index.js
@@ -16,6 +16,38 @@ Function.prototype.mockApply = function (target, args) {
     const result = obj[key](...args);
     delete obj[key];
     return result;
-}
+};
 
+/**
+ * 防抖
+ */
+export const debounce = function (func, wait) {
+    let timer = null;
+    return function (...args) {
+        const context = this;
+        timer && clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = null;
+            func.apply(context, args);
+        }, wait);
+    }
+};
 
+/**
+ * 节流
+ */
+
+export const throttle = function (func, wait) {
+    let startTime = 0;
+
+    return function (...args) {
+        const context = this;
+        const handTime = +new Date(); // 小技巧等于调用getTime方法
+
+        if (handTime - startTime > wait) {
+            func.apply(context, args);
+            startTime = handTime;
+        }
+
+    }
+};
