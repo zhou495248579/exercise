b3c393fe0ae5ff2b075849de9f46ae6311484485
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-14 18:49:50 +0800

    reduce结合短路操作handle undefined

--- src/pollfill/index.js
+++ src/pollfill/index.js
@@ -25,7 +25,7 @@ export const debounce = function (func, wait) {
     let timer = null;
     return function (...args) {
         const context = this;
-        timer && clearTimeout(timer);
+        timer && clearTimeout(timer); // 如果之前有定时器在等待，就撤销调用，每次定时器都保存最新的调用
         timer = setTimeout(() => {
             timer = null;
             func.apply(context, args);
@@ -53,15 +53,21 @@ export const throttle = function (func, wait) {
 };
 
 export const mockInstanceOf = function (l, r) {
-    if(typeof l !== 'object') {
+    if (typeof l !== 'object') {
         return false;
     }
 
-    while(l) {
-       if( l.__proto__ === r.prototype) {
-           return true;
-       }
-       l=l.__proto__;
+    while (l) {
+        if (l.__proto__ === r.prototype) {
+            return true;
+        }
+        l = l.__proto__;
     }
-    return  false;
+    return false;
+};
+
+export const get = function (params, obj) {
+    return params.reduce((accum, attr) => {
+        return (accum && accum[attr]) ? accum[attr] : null;
+    }, obj);
 };
