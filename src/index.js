62d6b6077223888191170931e4f0eb8b34168ccc
zhouyijun <zhouyijun@ruijie.com.cn>
2020-03-20 19:42:06 +0800

    flat 数组

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import html from './index.html'
 // import styles from './styles/index.less';
 
-// import './pollfill/index'
-import {Child} from "./pollfill/inherit";
+import './pollfill/index'
+import {flat} from "./pollfill";
 
-console.log(new Child())
+console.log(flat([1,[2,3], [4,[5, 6]]]))
