d57a37386fd56a1451274ee0cdf56690262e16f2
zhouyijun <zhou495248579@hotmail.com>
2020-08-01 12:14:52 +0800

    test case

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./sum";
+
+test("test 1+2", () => {
+  expect(sum(1, 2)).toBe(3);
+});
---
+++ src/sum.ts
@@ -0,0 +1,4 @@
+export function sum(p1: number, p2: number) {
+    return p1 + p2;
+  }
+  
