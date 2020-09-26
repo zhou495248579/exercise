8f27cb298f5a90ac31f684b1e122936b961f655e
zhouyijun <zhou495248579@hotmail.com>
2020-09-26 17:18:35 +0800

    申通笔试

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,5 @@
 import { sum } from "./sum";
+import {isSymmetricalClosed, queryParse} from "./sto-write-test";
 
 test("test 1+2", () => {
   expect(sum(1, 2)).toBe(3);
@@ -12,4 +13,43 @@ test("test to be undefined", () => {
 test("test tobe", () => {
   const obj = { name: "tom", age: 0 };
   expect(obj).toEqual({ name: "tom", age: 10 });
+});
+
+test("r", () => {
+  const sourceUrl = "https://www.taobao.com?a=1&b=2&c=3&d#name";
+  expect(queryParse(sourceUrl)).toEqual({
+    a: "1",
+    b: "2",
+    c: "3",
+    d: null,
+  });
+  expect(queryParse("https://www.taobao.com")).toEqual({});
+});
+
+test("s", () => {
+  expect(isSymmetricalClosed("1->2")).toBeFalsy();
+  expect(isSymmetricalClosed("1")).toBeTruthy();
+  expect(isSymmetricalClosed("1->5->3->5->1")).toBeTruthy();
+});
+
+test("r", () => {
+  const sourceUrl = "https://www.taobao.com?a=1&b=2&c=3&d#name";
+  expect(queryParse(sourceUrl)).toEqual({
+    a: "1",
+    b: "2",
+    c: "3",
+    d: null,
+  });
+  expect(queryParse("https://www.taobao.com")).toEqual({});
+});
+
+test("r", () => {
+  const sourceUrl = "https://www.taobao.com?a=1&b=2&c=3&d#name";
+  expect(queryParse(sourceUrl)).toEqual({
+    a: "1",
+    b: "2",
+    c: "3",
+    d: null,
+  });
+  expect(queryParse("https://www.taobao.com")).toEqual({});
 });
