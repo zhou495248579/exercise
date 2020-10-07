14c07ea5724bf40bb3bfcbc8b2df4462e487e72b
zhouyijun <zhou495248579@hotmail.com>
2020-10-07 11:25:22 +0800

    变量声明

---
+++ src/interview/2.js
@@ -0,0 +1,5 @@
+(function () {
+    var a = (b = 5); // 如果严格模式下 b需要写明 window.b
+})();
+console.log(b);
+console.log(a); // a定义在函数iife作用域内，b定义在window上
--- src/interview/a.js
+++ src/interview/a.js
@@ -3,3 +3,10 @@ var a = 1;
   a = 2;
   console.log(a); // iife中函数名不可变 a=2不会修改变量a，严格模式下回报错
 })();
+var a = [0];
+if (a) {
+  console.log(a == true);
+} else {
+  console.log(a);
+}
+
